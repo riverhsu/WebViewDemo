65521bcfada65ece45d1cd32120ddf1a349a1053
Sgmedical <sgmedical@sgs-macbook-air.local>
2016-10-13 16:40:12 +0800

    1. create web view, input text for http link and connect button 2. show the web site in web view correctly 3. notice, you have to permit the access of Internet in App

--- WebViewDemo/ViewController.swift
+++ WebViewDemo/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tHTTP: UITextField!
+    @IBOutlet weak var tWebView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func connectAction(_ sender: UIButton) {
+        let vlen: Int? = tHTTP.text?.characters.count
+        if vlen != nil {
+            let urlLink = NSURL(string: tHTTP.text!)
+            let urlreq = NSURLRequest(url: urlLink! as URL)
+            tWebView.loadRequest(urlreq as URLRequest)
+            
+        }
+    }
 
 }
 
